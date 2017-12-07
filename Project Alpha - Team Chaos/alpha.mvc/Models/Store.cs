3a9d60282925df9e11b149cc45d7ace62c58f2ae
Jenny Miderkvist <jennymider@outlook.com>
2017-12-07 11:43:24 +0100

    created new database

---
+++ Project Alpha - Team Chaos/alpha.mvc/Models/Store.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace alpha.mvc.Models
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+    }
+}
